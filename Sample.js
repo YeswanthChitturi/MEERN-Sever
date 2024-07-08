abc849dbf263610a7d9836f0e9b99733d362a81e
YeswanthChitturi <yeswanthkanna6281@gmail.com>
2024-07-08 13:26:14 +0530

    updates

--- Sample.js
+++ Sample.js
@@ -2,3 +2,5 @@ npm install mongodb
 npm install mongoose
 
 mongodb+srv://yeswanthias2030:<password>@cluster0.lnhhlgi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
+github token: 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
